3e9c2e8dd006459def70651452a66376127ebbf4
Sergey Burik <sergioburik@gmail.com>
2023-05-18 22:46:30 +0300

    Update regressions.py

--- regressions.py
+++ regressions.py
@@ -1,100 +1,55 @@
-points = [[5, 13], [4, 11], [7, 14], [9, 24], [10, 25]]
-
-cur = 10**10
-res = 10**10
-
-mnk = 0
-mnb = 0
-
-for k in range(-100, 100):
-	for b in range(-1000, 1000):
-		avg = 0
-		for point in points:
-			cur = abs(point[1] - (k*point[0] + b))
-			avg += cur
-		avg /= len(points)
-		if avg < res:
-			mnk = k
-			mnb = b
-			res = avg
-print(f"y = {mnk}x + {mnb}")	
-
-
-def f(x):
-	global mnk
-	global mnb
-	return mnk*x+mnb
-
+from random import randint
 import matplotlib.pyplot as plt
 
+n = 20
+k = randint(-100, 100)
+b = randint(-100, 100)
+# generate points
+points = [[i, k*i+b+randint(-int(abs(b*0.5)), int(abs(b*0.5)))] for i in range(n)]
+
+print("real k value:",k)
+avg_k = 0
+count = 0
+for i in range(len(points)):
+	for j in range(i+1, len(points)):
+		k = (points[i][1]- points[j][1]) / (points[i][0] - points[j][0])
+		avg_k += k
+		count += 1
+
+avg_k /= count
+
+avg_b = 0
+for point in points:
+	b = point[1] - (avg_k * point[0]) 
+	avg_b += b
+
+avg_b /= len(points)
+
+
+print(f"y = {avg_k}x + {avg_b}")	
+
+def f(x):
+	global avg_k
+	global avg_b
+	return avg_k*x+avg_b
+
+mae = 0
+for point in points:
+	mae = abs(abs(point[1]) - abs(f(point[0])))
+
+mae /= len(points)
+print("MAE (avg)", mae)
 
 points_sorted = sorted(points)
 
 x_0, x_1 = points_sorted[0][0], points_sorted[-1][0]
-print(x_0, x_1)
 x_reg = list(range(x_0, x_1+1))
 y_reg = [f(x) for x in x_reg]
 
 x = [point[0] for point in points]
 y = [point[1] for point in points]
 
-
 plt.scatter(x, y, c ="blue")
-plt.plot(x_reg, y_reg, linestyle='-')  # solid
+plt.plot(x_reg, y_reg, linestyle='-', c="red")  # solid
 
-# To show the plot
 plt.show()
-
-
-
-# import math
-
-# points = [[9, 2.95], [27, 4.2], [81, 5.1], [100, 5.4]]
-
-# cur = 10**10
-# res = 10**10
-
-# mnk = 0
-# mnb = 0
-# mnw = 0
-
-# for w in range(1, 2):
-# 	for k in range(2, 100):
-# 		for b in range(-100, 100):
-# 			avg = 0
-# 			for point in points:
-# 				cur = abs(abs(point[1]) - abs(w*math.log(point[0], k) + b))
-# 				avg += cur
-# 			avg /= len(points)
-# 			if avg < res:
-# 				mnk = k
-# 				mnb = b
-# 				mnw = w
-# 				res = avg
-# print(f"y = {mnw}*log{mnk}(x) + {mnb}")	
-# print(res)
-
-# def f(x):
-# 	global mnk
-# 	global mnb
-# 	return math.log(x, mnk) + mnb
-
-# import matplotlib.pyplot as plt
-
-
-# points_sorted = sorted(points)
-
-# x_0, x_1 = points_sorted[0][0], points_sorted[-1][0]
-# print(x_0, x_1)
-# x_reg = list(range(x_0, x_1+1))
-# y_reg = [f(x) for x in x_reg]
-
-# x = [point[0] for point in points]
-# y = [point[1] for point in points]
-
-
-# plt.scatter(x, y, c ="blue")
-# plt.plot(x_reg, y_reg, linestyle='-')  # solid
-
-# # To show the plot
-# plt.show()
